aa24387f5de9c1eda71613d524cc02cb590c3416
Axel <axelchandelier0@gmail.com>
2021-05-27 19:17:46 +0200

    build: builded the app for test

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import core from '@actions/core';
+const core_1 = __importDefault(require("@actions/core"));
 const lighthouse_1 = __importDefault(require("lighthouse"));
 const chrome_launcher_1 = require("chrome-launcher");
 const fs_1 = __importDefault(require("fs"));
@@ -27,8 +27,6 @@ function gatherResults(categories) {
     });
 }
 try {
-    // const urlsInput = core.getInput('urls');
-    // const urls = urlsInput.split(',');
     const fast4GOptions = {
         rttMs: 40,
         throughputKbps: 10 * 1024,
@@ -38,12 +36,16 @@ try {
         uploadThroughputKbps: 0,
     };
     (() => __awaiter(void 0, void 0, void 0, function* () {
+        const urlsInput = core_1.default.getInput('urls');
+        console.log(`urlsInput`, urlsInput);
+        const urls = urlsInput.split(',');
+        console.log('urls ->>', urls);
         const chrome = yield chrome_launcher_1.launch({
             chromeFlags: ['--headless'],
         });
         //* For all options, see https://github.com/GoogleChrome/lighthouse/blob/master/lighthouse-core/config/constants.js
         const options = {
-            logLevel: 'info',
+            logLevel: 'quiet',
             output: 'html',
             port: chrome.port,
             throttling: fast4GOptions,
@@ -57,7 +59,7 @@ try {
             formFactor: 'desktop',
             emulatedUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4420.0 Safari/537.36 Chrome-Lighthouse',
         };
-        const runnerResult = yield lighthouse_1.default('http://localhost:3000/fr', options);
+        const runnerResult = yield lighthouse_1.default(urls, options);
         // `.report` is the HTML report as a string
         const reportHtml = runnerResult.report;
         fs_1.default.writeFileSync('lhreport.html', reportHtml);
